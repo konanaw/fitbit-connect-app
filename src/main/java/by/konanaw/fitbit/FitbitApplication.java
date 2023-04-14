60ae2ab2e05b60bc1ea48666ac1bab4d8c99ff15
konanaw <94195724+konanaw@users.noreply.github.com>
2023-04-15 00:45:35 +0300

    second commit

---
+++ src/main/java/by/konanaw/fitbit/FitbitApplication.java
@@ -0,0 +1,13 @@
+package by.konanaw.fitbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/konanaw/fitbit/FitbitApplicationTests.java
@@ -0,0 +1,13 @@
+package by.konanaw.fitbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
